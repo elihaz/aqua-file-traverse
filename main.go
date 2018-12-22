f429e748f92bdda1cdb589e1bac9de36cbbf4f1d
elihaz <elihazanov@gmail.com>
2018-12-22 21:00:26 +0200

    Update main.go

--- main.go
+++ main.go
@@ -137,7 +137,7 @@ type FileInformation struct {
 
 func exists(path string) (bool, error) {
 
-	if _, err := os.Stat(path); !os.IsNotExist(err) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return false, err
 	}
 	return true, nil
